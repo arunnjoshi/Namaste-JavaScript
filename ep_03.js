30c738bef2ffdb396d0dc3e3f9d1cb8ba8bd0018
Arun Joshi <43997581+arunnjoshi@users.noreply.github.com>
2024-07-20 00:08:34 +0530

    #comment minor changes.

---
+++ ep_03.js
@@ -0,0 +1,12 @@
+// getName('arun Joshi');
+console.log(typeof (getName));
+
+
+console.log(getName);
+console.log(typeof (x));
+var x = 10;
+var getName = (name) => {
+    console.log(`${name}`);
+}
+
+console.log(typeof (getName));
