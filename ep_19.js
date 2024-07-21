f5a2c9b602a86b7173f6f4d2814eb740e53279fe
Arun Joshi <43997581+arunnjoshi@users.noreply.github.com>
2024-07-21 11:35:24 +0530

    #comment change.

--- ep_19.js
+++ ep_19.js
@@ -2,12 +2,12 @@ const arr = [1, 2, 3, 4, 5]
 
 // map 
 const output = arr.map(x => ({ 'double': 2 * x, 'triple': x * 3 }));
-console.log(output);
+// console.log(output);
 
 // filter
 
 var filter = output.filter(x => x.double > 6)
-console.log(filter);
+// console.log(filter);
 
 // reduce
 
@@ -15,4 +15,16 @@ var reduce = output.reduce((acc, curr) => {
     acc.double = curr.double + acc.double
     return acc;
 }, { double: 0 })
+// console.log(reduce);
+
+
+var reduce = [{ double: 1 }, { double: 11 }, { double: 9 }, { double: 8 }].reduce((acc, curr) => {
+    if (acc.double <= curr.double) {
+        acc.min = acc.double
+    }
+    else {
+        acc.min = curr.double;
+    }
+    return acc;
+})
 console.log(reduce);
